fd9a7d1ce5d089f324ab46355864e960ac1c5aa4
atalaman <talamtolik@yandex.ru>
2019-06-05 01:16:31 +0300

    Fix comments to review

--- include/string_fragmentator.hpp
+++ include/string_fragmentator.hpp
@@ -3,42 +3,35 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <algorithm>
+#include <iostream>
 
 struct Fragment final
 {
     Fragment() = default;
-    Fragment(uint16_t, uint16_t, uint16_t, uint16_t, const char*);
+    Fragment(uint16_t id, uint16_t fid, uint16_t total, uint16_t size, std::string data)
+        : _id(id), _fid(fid), _total(total), _size(size), _data(std::move(data)) {
+    }
 
-    Fragment(const Fragment& other);
-    Fragment(Fragment&&) noexcept;
+    uint16_t id()    const { return _id;    }
+    uint16_t fid()   const { return _fid;   }
+    uint16_t total() const { return _total; }
+    uint16_t size()  const { return _size;  }
 
-    Fragment& operator=(const Fragment&);
-    Fragment& operator=(Fragment&&) noexcept;
+    // Only read access
+    const std::string&  data() const &  { return _data; }
+    const std::string&  data() &        { return _data; }
 
-    uint16_t    id()    const { return _id;    }
-    uint16_t    fid()   const { return _fid;   }
-    uint16_t    total() const { return _total; }
-    uint16_t    size()  const { return _size;  }
-    const char* data()  const { return _data;  }
-
-    ~Fragment();
+    const std::string&& data() const && = delete;
+          std::string&& data() &&       { return std::move(_data); }
 
 private:
-    void MakeCopy(const Fragment&);
-    void MakeMove(Fragment&&) noexcept;
-
-    void AllocateAndFillData(const char*, uint16_t);
-    void CopySimpleFields(const Fragment&) noexcept;
-
-    void Destroy() noexcept;
-
-    uint16_t _id;
-    uint16_t _fid;
-    uint16_t _total;
-    uint16_t _size;
-    char*    _data;
+    uint16_t    _id;
+    uint16_t    _fid;
+    uint16_t    _total;
+    uint16_t    _size;
+    std::string _data;
 };
-
 
 class StringFragmentator final
 {
@@ -51,35 +44,53 @@ public:
 
     std::vector<Fragment> Split(const std::vector<std::string>& strs, size_t num_fragments);
 
-    const std::unordered_map<int, std::string>& GetStringMap() const;
-private:
-    std::unordered_map<int, std::string>           _id_to_strs;
-    std::unordered_map<int, std::vector<Fragment>> _id_to_fragments;
+    const std::unordered_map<int, std::string>&  GetStringMap() &        { return _id_to_strs;            }
+    const std::unordered_map<int, std::string>&  GetStringMap() const &  { return _id_to_strs;            }
 
-    // FIXME Make Singletone or static
+    const std::unordered_map<int, std::string>&& GetStringMap() const && = delete;
+          std::unordered_map<int, std::string>&& GetStringMap() &&       { return std::move(_id_to_strs); }
+
+
+private:
+    /* FIXME Struct has not trivial logic with increment num_received_fragments and clearing state,
+     * maybe better use class ? But now it internal only stuff and used in one place */
+    struct FragmentsInfo {
+        FragmentsInfo() = default;
+        explicit FragmentsInfo(int num_fragments) : fragments(num_fragments) {};
+        size_t num_received_fragments = 0;
+        std::vector<std::string> fragments;
+    };
+
+    std::unordered_map<int, std::string  > _id_to_strs;
+    std::unordered_map<int, FragmentsInfo> _id_to_finfo;
+
     int _id_counter = 0;
 };
 
 template<typename T>
 void StringFragmentator::AddFragment(T&& f) {
-    // NB: Create and reserve if needed
-    _id_to_fragments[f.id()].reserve(f.total());
+    if (auto it = _id_to_finfo.find(f.id()); it == _id_to_finfo.end()) {
+        _id_to_finfo.emplace(f.id(), f.total());
+    }
+
+    auto& all_finfos = _id_to_finfo[f.id()];
 
     // NB: If string is already collected, remove it and start to collect again
-    if (_id_to_fragments[f.id()].size() == f.total()) {
-        _id_to_fragments[f.id()].clear();
+    if (all_finfos.num_received_fragments == f.total()) {
+        all_finfos.num_received_fragments = 0;
         _id_to_strs.erase(f.id());
     }
 
-    _id_to_fragments[f.id()].push_back(std::forward<T>(f));
+    all_finfos.fragments[f.fid()] = std::forward<T>(f).data();
+    all_finfos.num_received_fragments++;
 
-    if (_id_to_fragments[f.id()].size() == f.total()) {
-        auto& str = _id_to_strs[f.id()];
-        str.reserve(f.total() * f.size() + f.size());
-        const auto& all_fragments = _id_to_fragments[f.id()];
-
-        for (const auto& fragment : all_fragments) {
-            str.append(fragment.data(), fragment.size());
+    if (all_finfos.num_received_fragments == f.total()) {
+        std::string total_str;
+        total_str.reserve(f.total() * f.size() + f.size());
+        for (const auto& s : all_finfos.fragments) {
+            total_str.append(s);
         }
+
+        _id_to_strs.emplace(f.id(), std::move(total_str));
     }
 }
--- tests/string_fragmentator_tests.cpp
+++ tests/string_fragmentator_tests.cpp
@@ -1,89 +1,11 @@
 #include <string>
 #include <vector>
+#include <random>
+#include <algorithm>
 
 #include <gtest/gtest.h>
 
 #include "string_fragmentator.hpp"
-
-TEST(Fragment, CreateDefault) {
-    EXPECT_NO_THROW(Fragment());
-}
-
-TEST(Fragment, CreateInit) {
-    Fragment f(0, 1, 2, 4, "TEST");
-
-    EXPECT_EQ(0, f.id());
-    EXPECT_EQ(1, f.fid());
-    EXPECT_EQ(2, f.total());
-    EXPECT_EQ(4, f.size());
-    EXPECT_EQ(0, strncmp("TEST", f.data(), f.size()));
-}
-
-TEST(Fragment, CreateCopy) {
-    Fragment f1(0, 1, 2, 4, "TEST");
-    Fragment f2(f1);
-
-    EXPECT_EQ(f1.id()   , f2.id());
-    EXPECT_EQ(f1.fid()  , f2.fid());
-    EXPECT_EQ(f1.total(), f2.total());
-    EXPECT_EQ(f1.size() , f2.size());
-    EXPECT_EQ(0, strncmp(f1.data(), f2.data(), f1.size()));
-}
-
-TEST(Fragment, CreateMove) {
-    Fragment f1(0, 1, 2, 4, "TEST");
-    Fragment f2(std::move(f1));
-
-    EXPECT_EQ(0, f2.id());
-    EXPECT_EQ(1, f2.fid());
-    EXPECT_EQ(2, f2.total());
-    EXPECT_EQ(4, f2.size());
-    EXPECT_EQ(0, strncmp("TEST", f2.data(), f1.size()));
-    EXPECT_EQ(nullptr, f1.data());
-}
-
-TEST(Fragment, CopyAssign) {
-    Fragment f1(0, 1, 2, 5, "CORGE");
-    Fragment f2(1, 2, 3, 4, "QUUX ");
-
-    f2 = f1;
-
-    EXPECT_EQ(f1.id()   , f2.id());
-    EXPECT_EQ(f1.fid()  , f2.fid());
-    EXPECT_EQ(f1.total(), f2.total());
-    EXPECT_EQ(f1.size() , f2.size());
-    EXPECT_EQ(0, strncmp(f1.data(), f2.data(), f1.size()));
-}
-
-TEST(Fragment, SelfCopyAssign) {
-    Fragment f(0, 1, 2, 4, "TEST");
-
-    f = f;
-
-    EXPECT_EQ(0, f.id());
-    EXPECT_EQ(1, f.fid());
-    EXPECT_EQ(2, f.total());
-    EXPECT_EQ(4, f.size());
-    EXPECT_EQ(0, strncmp("TEST", f.data(), f.size()));
-}
-
-TEST(Fragment, SelfMoveAssign) {
-    Fragment f(0, 1, 2, 4, "TEST");
-
-    f = std::move(f);
-
-    EXPECT_EQ(0, f.id());
-    EXPECT_EQ(1, f.fid());
-    EXPECT_EQ(2, f.total());
-    EXPECT_EQ(4, f.size());
-    EXPECT_EQ(0, strncmp("TEST", f.data(), f.size()));
-}
-
-TEST(Fragment, CorrectDestroy) {
-    auto* f = new Fragment(0, 1, 2, 4, "TEST");
-
-    EXPECT_NO_THROW(delete f);
-}
 
 // FIXME Add more tests, and avoid code dublicate (Define TestFixture)
 TEST(StringFragmentator, SplitOneWithEvenSize)
@@ -103,7 +25,7 @@ TEST(StringFragmentator, SplitOneWithEvenSize)
         EXPECT_EQ(answers[i][1], fragments[i].fid());
         EXPECT_EQ(answers[i][2], fragments[i].total());
         EXPECT_EQ(answers[i][3], fragments[i].size());
-        EXPECT_EQ(0, strcmp(data_ans[i].data(), fragments[i].data()));
+        EXPECT_EQ(data_ans[i].data(), fragments[i].data());
     }
 }
 
@@ -123,7 +45,7 @@ TEST(StringFragmentator, SplitOneWithOddSize)
         EXPECT_EQ(answers[i][1], fragments[i].fid());
         EXPECT_EQ(answers[i][2], fragments[i].total());
         EXPECT_EQ(answers[i][3], fragments[i].size());
-        EXPECT_EQ(0, strcmp(data_ans[i].data(), fragments[i].data()));
+        EXPECT_EQ(data_ans[i].data(), fragments[i].data());
     }
 }
 
@@ -134,14 +56,6 @@ TEST(StringFragmentator, FragmentsMoreThanSymbols)
     size_t num_fragments = 10;
 
     EXPECT_ANY_THROW(sf.Split(strs, num_fragments));
-}
-
-TEST(StringFragmentator, EmptyStringSet)
-{
-    StringFragmentator sf;
-    std::vector<std::string> strs;
-
-    EXPECT_ANY_THROW(sf.Split(strs, 4));
 }
 
 TEST(StringFragmentator, NumFragmentsMoreThanZero)
@@ -172,6 +86,25 @@ TEST(StringFragmentator, ConcatMoreOneStrings)
     StringFragmentator sf;
     std::vector<std::string> strs = { "TEST", "BAZ", "FOO", "QUUX", "CORGE" };
     auto fragments = sf.Split(strs, 3);
+
+    sf.AddFragments(fragments);
+
+    const auto& str_map = sf.GetStringMap();
+
+    EXPECT_EQ(5u, str_map.size());
+    for (int i = 0; i < strs.size(); ++i) {
+        EXPECT_EQ(strs[i], str_map.at(i));
+    }
+}
+
+TEST(StringFragmentator, UnorderedFragments)
+{
+    StringFragmentator sf;
+    std::vector<std::string> strs = { "TEST", "BAZ", "FOO", "QUUX", "CORGE" };
+    auto fragments = sf.Split(strs, 3);
+
+    std::mt19937 g{std::random_device()()};
+    std::shuffle(fragments.begin(), fragments.end(), g);
 
     sf.AddFragments(fragments);
 
